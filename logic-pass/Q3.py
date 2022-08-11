e629bb99f07651670f2428ef4258cee31769efb7
Mohammed2_14 <85359100+M-STRONG@users.noreply.github.com>
2022-08-11 23:42:04 +0300

    Create Q3.py

---
+++ logic-pass/Q3.py
@@ -0,0 +1,10 @@
+def counter():
+            str = "Artificial intelligence"
+            count = 0
+            for n in str:
+                #delet "i", add your any char
+                if n == 'i':
+                    count = count + 1
+            print (count)
+
+print(counter())
